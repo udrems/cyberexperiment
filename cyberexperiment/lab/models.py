7e1117997b0766f8f6145875aa327b0588e803b2
Chidozie C. Okafor <chidosiky2015@gmail.com>
2021-09-12 19:55:08 +0100

    ✅ adding lab and follower app

--- cyberexperiment/lab/models.py
+++ cyberexperiment/lab/models.py
@@ -1,3 +1,42 @@
-# from django.db import models
+from django.conf import settings
+from django.db import models
 
-# Create your models here.
+
+class TimeStamp(models.Model):
+    """time stamp model for inserting created at and modified time"""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class MembershipStatus(TimeStamp):
+    name = models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
+
+
+class LabMembership(TimeStamp):
+    slug = models.SlugField(max_length=100)
+    membership_type = models.ForeignKey(MembershipStatus, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
+    payment_plan_id = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.membership_type.name
+
+
+class UserLabMembership(TimeStamp):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    payment_customer_id = models.CharField(max_length=50)
+    membership_type = models.ForeignKey(LabMembership, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+
+class LabSubscription(TimeStamp):
+    active = models.BooleanField(default=False)
