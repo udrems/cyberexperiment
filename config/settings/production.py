f2ce5d98f006bcb84a9d055b8d170e803f9fb281
Chidozie C. Okafor <chidosiky2015@gmail.com>
2021-09-19 06:50:20 +0100

    ⚡️ change production ALLoWED_HoST

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["cyberexperiment.com"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=["cyberexperiment.com", "cyberexperiment.herokuapp.com"],
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
