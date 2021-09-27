383f5b30a87c85a3949b724a6b79d4c8c443700f
Chidozie C. Okafor <chidosiky2015@gmail.com>
2021-09-27 15:47:39 +0100

    🔥 removing lab_course

--- cyberexperiment/core/models.py
+++ cyberexperiment/core/models.py
@@ -1,13 +1,48 @@
+from django.conf import settings
 from django.db import models
+from django.urls import redirect
 
 
 class TimeStamp(models.Model):
-    """timestamp"""
+    """timestamp to capture all the time of event in the database"""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
 
-class Genera(models.Model):
+# general model
+class General(models.Model):
+    """General model enable other models similar fields to be inherited. it is use to enforce dry principle"""
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=50, unique=True)
+    detail = models.TextField(blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
     class Meta:
         abstract = True
+
+
+class Blog(TimeStamp, General):
+    excerpt = models.TextField(blank=True)
+    view_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self, *args, **kwargs):
+        return redirect("core:detail", kwargs={"slug": self.slug})
+
+
+class Event(TimeStamp, General):
+    excerpt = models.TextField(blank=True)
+    view_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self, *args, **kwargs):
+        return redirect("core:detail", kwargs={"slug": self.slug})
--- cyberexperiment/lab/models.py
+++ cyberexperiment/lab/models.py
@@ -1,15 +1,15 @@
+from core.models import General, TimeStamp
 from django.conf import settings
 from django.db import models
 
+# class TimeStamp(models.Model):
+#     """time stamp model for inserting created at and modified time"""
 
-class TimeStamp(models.Model):
-    """time stamp model for inserting created at and modified time"""
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     modified_at = models.DateTimeField(auto_now=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
+#     class Meta:
+#         abstract = True
 
 
 class MembershipStatus(TimeStamp):
@@ -45,3 +45,41 @@ class LabSubscription(TimeStamp):
 
     def __str__(self):
         return self.user_membership.user.username
+
+
+class Lab(TimeStamp, General):
+    """Before a user create a content, he must host a lab first"""
+
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+    def __init__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Lab"
+
+
+class LabCourse(TimeStamp, General):
+    """This enable creators create courses and experiment others can follow"""
+
+    lab = models.ForeignKey(Lab, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Lab Content"
+
+
+class Lesson(TimeStamp, General):
+    """lessons will be linked to a lab course that will enable the users to create multiple lessons"""
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Lesson"
+
+
+class MultiImage(TimeStamp):
+    """This will enable creating multiple images on the model"""
