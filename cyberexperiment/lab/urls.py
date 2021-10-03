e7bc71957e59c69cf20833b6fc56af5cbd310c77
Chidozie C. Okafor <chidosiky2015@gmail.com>
2021-10-03 15:29:25 +0100

    ✨ creators dashboard and profile

--- cyberexperiment/lab/urls.py
+++ cyberexperiment/lab/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from .views import CreateCourse
 
 app_name = "lab"
-urlpatterns = [path("content/", CreateCourse.as_view(), name="create_course")]
+urlpatterns = [
+    path("content/", CreateCourse.as_view(), name="create_course"),
+]
