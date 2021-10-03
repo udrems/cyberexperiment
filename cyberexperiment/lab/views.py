e0d26aad49d5fc20967928ee847ac06675597087
Chidozie C. Okafor <chidosiky2015@gmail.com>
2021-10-03 17:09:16 +0100

    🚧 working on messages, notification, feedback etc

--- cyberexperiment/lab/views.py
+++ cyberexperiment/lab/views.py
@@ -4,3 +4,27 @@ from django.views import generic
 
 class CreateCourse(generic.TemplateView):
     template_name = "circus/create_new_course.html"
+
+
+class MyContent(generic.TemplateView):
+    template_name = "circus/my_contents.html"
+
+
+class Analytic(generic.TemplateView):
+    template_name = "circus/analytic.html"
+
+
+class Message(generic.TemplateView):
+    template_name = "circus/messages.html"
+
+
+class Notification(generic.TemplateView):
+    template_name = "circus/notification.html"
+
+
+class Review(generic.TemplateView):
+    template_name = "circus/review.html"
+
+
+class FeedBack(generic.TemplateView):
+    template_name = "circus/feedback.html"
